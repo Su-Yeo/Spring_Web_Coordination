23d7086d2d95ec128fc4c37ebc4642c80420b85d
Suhyang <yyy1247@gmail.com>
2019-11-25 23:55:59 +0900

    서비스키 지움 - 공공데이터 포털 (신)동네예보정보조회서비스 서비스키 받아야 함

--- Coordination_MVC/java/com/coordination/weather/ApiExplorerWeather.java
+++ Coordination_MVC/java/com/coordination/weather/ApiExplorerWeather.java
@@ -1,4 +1,4 @@
-package com.coordination.weather;
+﻿package com.coordination.weather;
 
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -32,10 +32,8 @@ public class ApiExplorerWeather {
         	baseDate=date.format(today);
         }
        
-        //인증키1 : qenZlJjZb6XaqFBDSk%2B6BE6hSgp4HMzlVHjuZV%2FN07OXNyH4dHxyXjyH3qtq8LyC6EC%2Bd1gOg0Tm2gR%2FusFSWQ%3D%3D
-        //인증키2 : KaI%2FcewqprPhkm8XnSeaZV5lfBAZAiQSe0RCCjYJG7UDgj5a5ChYmXLG5GObAWG%2BxoBQC0HESNXkd88FtcAR1A%3D%3D
         StringBuilder urlBuilder = new StringBuilder("http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=KaI%2FcewqprPhkm8XnSeaZV5lfBAZAiQSe0RCCjYJG7UDgj5a5ChYmXLG5GObAWG%2BxoBQC0HESNXkd88FtcAR1A%3D%3D"); /*Service Key*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=SERVICE_KEY"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode("TEST_SERVICE_KEY", "UTF-8")); /*서비스 인증*/
         urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /*‘15년 12월 1일발표*/
         urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0200", "UTF-8")); /*02시 발표 * 기술문서 참조*/
